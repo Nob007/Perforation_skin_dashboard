aaf9dc3897f9b897d3e7ab41a236af3da00ed28e
Nob007 <soumikdutta9a2020@gmail.com>
2026-03-07 04:26:28 +0530

    Create 1_Flow_efficiency.py

---
+++ pages/1_Flow_efficiency.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title("Flow Efficiency")
+col1, col2, col3 = st.columns(3)
+re= col1.number_input("Drainage radius (ft)", min = 0.0, value = 630.0)
+rw=col2.number_input("Well Radius (in)", min = 0.0, value = 4.25)
+s=col3.number_input("Skin", value = 1.5)
+
+Flow_efficiency = np.log(re*12.0/rw)/(np.log(re*12/rw) + s)
+
+st.metric("Flow Efficiency", value = f"{Flow_efficiency}")
