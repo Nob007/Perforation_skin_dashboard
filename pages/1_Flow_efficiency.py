c0e1610cecb7ab023b015a9c7c58bd1288cb44a6
Nob007 <soumikdutta9a2020@gmail.com>
2026-03-07 04:28:58 +0530

    Update 1_Flow_efficiency.py

--- pages/1_Flow_efficiency.py
+++ pages/1_Flow_efficiency.py
@@ -4,8 +4,8 @@ import numpy as np
 
 st.title("Flow Efficiency")
 col1, col2, col3 = st.columns(3)
-re= col1.number_input("Drainage radius (ft)", min = 0.0, value = 630.0)
-rw=col2.number_input("Well Radius (in)", min = 0.0, value = 4.25)
+re= col1.number_input("Drainage radius (ft)", min_value = 0.0, value = 630.0)
+rw=col2.number_input("Well Radius (in)", min_value = 0.0, value = 4.25)
 s=col3.number_input("Skin", value = 1.5)
 
 Flow_efficiency = np.log(re*12.0/rw)/(np.log(re*12/rw) + s)
